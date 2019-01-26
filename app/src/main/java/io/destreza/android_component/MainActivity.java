c84ce3140f777aa0b36906472de4174b945ea791
Bellal Hosain <bellalhrbubt@gmail.com>
2019-01-26 16:55:33 +0600

    Add ratingBar and Seekbar

--- app/src/main/java/io/destreza/android_component/MainActivity.java
+++ app/src/main/java/io/destreza/android_component/MainActivity.java
@@ -2,12 +2,56 @@ package io.destreza.android_component;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.seckbar);
+
+       // getRatingBar();
+
+        getSeekbar();
     }
+
+    private void getRatingBar() {
+        Button ratinBtn=findViewById(R.id.getRatingBtn);
+        final RatingBar ratingBar=findViewById(R.id.ratingBar);
+
+        ratinBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float rating=ratingBar.getRating();
+                Toast.makeText(MainActivity.this, "Rating "+rating, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void getSeekbar(){
+        SeekBar seekBar=findViewById(R.id.seekbar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Toast.makeText(MainActivity.this, "OnProgressChanged "+progress, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(MainActivity.this, "OnProgressChanged "+seekBar.toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(MainActivity.this, "OnProgressChanged "+seekBar.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
 }

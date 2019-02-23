2d6899c568bc80462b5ad0d5cb740d3aa09fb6d3
Bellal Hosain <bellalhrbubt@gmail.com>
2019-02-23 12:47:15 +0600

    Maked room db for sqlite

---
+++ app/src/main/java/io/destreza/android_component/Room/RoomActivity.java
@@ -0,0 +1,15 @@
+package io.destreza.android_component.Room;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.destreza.android_component.R;
+
+public class RoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room);
+    }
+}
---
+++ app/src/main/java/io/destreza/android_component/Room/User.java
@@ -0,0 +1,14 @@
+package io.destreza.android_component.Room;
+
+@Entity
+public class User {
+
+    @PrimaryKey
+    public int uid;
+
+    @ColumnInfo(name = "first_name")
+    public String firstName;
+
+    @ColumnInfo(name = "last_name")
+    public String lastName;
+}
